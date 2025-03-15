fa4ee75d6ac045b7ce5e83e62ab5ae583fc2d304
Vaibhav Chopde <vchopde77@gmail.com>
2025-03-15 18:54:48 +0530

    feat: Added Read by Title, Tag

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ async function createNewEvent(eventData) {
 
 app.post("/events", async(req, res) => {
     try {
-        const newAdded = await createEvent(req.body)
+        const newAdded = await createNewEvent(req.body)
         res.status(201).json({message: "Event added successfully!", newEvent: newAdded})
     } catch (error) {
         res.status(500).json({error: "Error fetching meetup events. Try again !"})
@@ -38,6 +38,7 @@ app.post("/events", async(req, res) => {
 })
 
 // Read
+// Read All
 async function readAllEvents() {
     try {
         const events = await Event.find()
@@ -50,7 +51,54 @@ async function readAllEvents() {
 app.get("/events", async(req, res) => {
     try {
         const events = await readAllEvents()
-        if(events.lenth !=0) {
+        if(events.length !=0) {
+            res.json(events)
+        } else {
+            res.status(404).json({error: "Events not found!"})
+        }
+    } catch (error) {
+        res.status(500).json({error: "Error fetching meetup events. Try again !"})
+    }
+})
+
+// Read By Title
+async function readByTitle(eventTitle) {
+    try {
+        const events = await Event.findOne({title: eventTitle})
+        return events
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+app.get("/events/:eventTitle", async(req, res) => {
+    try {
+        const event = await readByTitle(req.params.eventTitle)
+        if(event) {
+            res.json(event)
+        } else {
+            res.status(404).json({error: "Event not found!"})
+        }
+    } catch (error) {
+        res.status(500).json({error: "Error fetching meetup event. Try again !"})
+    }
+})
+
+
+// Read by Tag
+async function readByTag(eventTag) {
+    try {
+        const events = await Event.find({tags: eventTag})
+        return events
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+app.get("/events/tags/:eventTag", async(req, res) => {
+    try {
+        const events = await readByTag(req.params.eventTag)
+        if(events.length != 0) {
             res.json(events)
         } else {
             res.status(404).json({error: "Events not found!"})
